f45b0afdfc80f314e4bd26147fc50ca8a7c4d28b
Josh Kadis <josh@kadisco.com>
2023-01-07 19:22:10 -0500

    setup athlete api route

--- pages/api/v3/athletes/[...athlete].js
+++
@@ -1,9 +0,0 @@
-import dbConnect from "../../../../lib/db/dbConnect";
-import Athlete from "../../../../lib/db/Athlete";
-
-export default async function handler(req, res) {
-  await dbConnect();
-  const athlete = await Athlete.findById(541773);
-  console.log("hi");
-  res.json(athlete.toJSON());
-}
---
+++ pages/api/v3/athletes/[athleteId].js
@@ -0,0 +1,22 @@
+import models from "../../../../lib/db/models";
+import APIAthlete from "../../../../lib/api/models/APIAthlete";
+
+const { Athlete } = models();
+
+export default async function handler(req, res) {
+  const { athleteId = "" } = req.query;
+
+  if (!athleteId.toString().match(/^\d+$/)) {
+    res
+      .status(400)
+      .json({ error: `Malformed athlete ID: ${req.query.athleteId}` });
+    return;
+  }
+
+  const athleteDoc = await Athlete.findById(athleteId);
+  if (!athleteDoc) {
+    res.status(404).json({ error: `Athlete ID not found: ${athleteId}` });
+  }
+
+  res.status(200).json(APIAthlete(athleteDoc));
+}
