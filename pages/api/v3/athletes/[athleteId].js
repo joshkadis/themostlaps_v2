dc448a35a62f3766ae03ce064558f18615cffa4c
Josh Kadis <josh@kadisco.com>
2023-01-15 21:54:37 -0500

    API routes error handling

--- pages/api/v3/athletes/[athleteId].js
+++ pages/api/v3/athletes/[athleteId].js
@@ -3,20 +3,23 @@ import APIAthlete, { projection } from "lib/api/models/APIAthlete";
 
 const { Athlete } = models();
 
-export default async function handler(req, res) {
-  const { athleteId = "" } = req.query;
+export default async function handler({ query = {} }, res) {
+  const { athleteId = "" } = query;
 
   if (!athleteId.toString().match(/^\d+$/)) {
-    res
-      .status(400)
-      .json({ error: `Malformed athlete ID: ${req.query.athleteId}` });
+    res.status(400).json({ error: `Malformed athlete ID: ${query.athleteId}` });
     return;
   }
 
-  const athleteDoc = await Athlete.findById(athleteId, projection);
-  if (!athleteDoc) {
-    res.status(404).json({ error: `Athlete ID not found: ${athleteId}` });
-  }
+  try {
+    const athleteDoc = await Athlete.findById(athleteId, projection);
+    if (!athleteDoc) {
+      res.status(404).json({ error: `Athlete ID not found: ${athleteId}` });
+    }
 
-  res.status(200).json(APIAthlete(athleteDoc));
+    res.status(200).json(APIAthlete(athleteDoc));
+  } catch (error) {
+    console.error(error.toString(), query);
+    res.status(500).json({ error: "An error occurred" });
+  }
 }
--- pages/api/v3/stats/rankings/locations/[location].js
+++ pages/api/v3/stats/rankings/locations/[location].js
@@ -82,19 +82,24 @@ async function rankingForLocation(query) {
   return { status: 200, ranking: athleteDocs.map(APIAthlete) };
 }
 
-export default async function handler({ query }, res) {
-  const { status, ranking } = await rankingForLocation(query);
+export default async function handler({ query = {} }, res) {
+  try {
+    const { status, ranking } = await rankingForLocation(query);
 
-  if (status === 404) {
-    res.status(404).json({ error: `Unknown location: ${location}` });
-    return;
-  }
-  if (status === 400) {
-    res
-      .status(400)
-      .json({ error: `Malformed query: ${JSON.stringify(query)}` });
-    return;
-  }
+    if (status === 404) {
+      res.status(404).json({ error: `Unknown location: ${location}` });
+      return;
+    }
+    if (status === 400) {
+      res
+        .status(400)
+        .json({ error: `Malformed query: ${JSON.stringify(query)}` });
+      return;
+    }
 
-  res.status(200).json(ranking);
+    res.status(200).json(ranking);
+  } catch (error) {
+    console.error(error.toString(), query);
+    res.status(500).json({ error: "An error occurred" });
+  }
 }
